c00ba58da69bf1d4ae6ae103af512847ca41fbc6
Jhorman Nieto P <jhosmita9@gmail.com>
2024-10-18 16:01:34 -0500

    Agregando metodo de autenticacion con ejs y node, creando asi una interfaz manejado directamente desde el servidor

---
+++ README.md
@@ -0,0 +1,15 @@
+
+Qué veremos hoy
+
+1. Instalación del proyecto
+2. Usuarios en base de datos
+3. Registro de usuarios
+4. Autenticación básico con login y password
+5. Usando hashes y salts
+6. Sesión de usuarios con cookie
+7. Rutas protegidas
+8. Json Web Token
+9. Refactorización con un middleware
+10. Cerrar sesión
+11. Refresh Token
+12. Más adelante: Passport y Oauth 2.0
---
+++ app.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import { PORT } from './utils/config.js';
+import { UserRepository } from './user-repository.js';
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await UserRepository.login({ username, password });
+        res.send({ user });
+    } catch (error) {
+        res.status(401).send(error.message);
+    }
+});
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const id = await UserRepository.create({ username, password });
+        res.send({id: id});
+    } catch (error) {
+        // NORMALMENTE NO ES BUENA IDEA MANDAR EL ERROR DEL REPOSITORIO
+        if(error.message)
+        res.status(404).send(error.message);
+    }
+});
+app.post('/logout', (req, res) => {});
+app.get('/protected', (req, res) => {
+    //TODO: if sesion del usuario
+    res.render('protected');
+    // TODO: else 401
+});
+
+
+app.listen(PORT, (req, res) => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});

b131c13dc4e5844556e19f5571ad46f5ce64b869
Jhorman Nieto P <jhosmita9@gmail.com>
2024-11-06 13:36:59 -0500

    Registro y login completado, con cookies

--- app.js
+++ app.js
@@ -1,23 +1,51 @@
 import express from 'express';
-import { PORT } from './utils/config.js';
+import jwt from 'jsonwebtoken';
+import { PORT, SECRET_JWT_KEY } from './utils/config.js';
 import { UserRepository } from './user-repository.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(express.json());
+app.use(cookieParser());
+app.use((req, res, next) => {
+    const token = req.cookies.access_token;
+    req.session = { user: null };
+
+    try {
+        const data = jwt.verify(token, SECRET_JWT_KEY);
+        req.session.user = data;
+    } catch {}
+
+    next();
+});
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const { user } = req.session;
+    res.render('index', user);
 });
+
+
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
     try {
         const user = await UserRepository.login({ username, password });
-        res.send({ user });
+        const token = jwt.sign({ id: user._id, username: user.username }, SECRET_JWT_KEY, {
+            expiresIn: '1h',
+            algorithm: 'HS256'
+        });
+        res
+        .cookie('access_token', token, {
+            httpOnly: true, // La cookie solo se puede acceder en el servidor
+            secure: process.env.NODE_ENV === 'production', // La cookie se enviará al cliente solo si la conexión es HTTPS
+            sameSite: 'strict', // La cookie solo se enviará al cliente si la conexión es del mismo dominio
+            maxAge: 1000 * 60 * 60
+        })
+        .send({ user, token });
     } catch (error) {
         res.status(401).send(error.message);
     }
@@ -34,11 +62,14 @@ app.post('/register', async (req, res) => {
         res.status(404).send(error.message);
     }
 });
-app.post('/logout', (req, res) => {});
+app.post('/logout', (req, res) => {
+    res.clearCookie('access_token')
+    res.send('Logged out');
+});
 app.get('/protected', (req, res) => {
-    //TODO: if sesion del usuario
-    res.render('protected', { username: 'jhormanDev' });
-    // TODO: else 401
+    const { user } = req.session;
+    if(!user) return res.status(403).send('Access not authorized');
+    res.render('protected', user);
 });
 
 
--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,5 @@
 export const { 
     PORT = 3000,
-    SALT_ROUNDS = 10 
+    SALT_ROUNDS = 10,
+    SECRET_JWT_KEY = 'this-is-a-an-awesome-secret-key-mucho-mas-largo-y-muy-seguro',
 } = process.env;
